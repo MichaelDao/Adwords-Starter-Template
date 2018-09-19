e01d79da8ae56a5d8a48eaa4e1530c205d6b793f
MichaelDao <daoduymichael@gmail.com>
2018-09-19 13:53:41 +1000

    overhaul template

--- circle/circleShine/ad.js
+++
@@ -1,4 +0,0 @@
-/* Prevent hover for 7.2 seconds */
-setTimeout(function () {
-    $('#blocker').remove();
-}, 7200);
--- slideIn/slideInShine/ad.js
+++
@@ -1,4 +0,0 @@
-/* Prevent hover for 6.5 seconds */
-setTimeout(function () {
-    $('#blocker').remove();
-}, 6500);
